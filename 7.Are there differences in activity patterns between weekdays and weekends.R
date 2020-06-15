b2dabac6163e944a81f52bd1a6d9c7a851adb66e
ojasvibhasin <65826071+ojasvibhasin@users.noreply.github.com>
2020-06-15 23:08:35 +0530

    Create 7.Are there differences in activity patterns between weekdays and weekends.R

---
+++ 7.Are there differences in activity patterns between weekdays and weekends.R
@@ -0,0 +1,11 @@
+7.Are there differences in activity patterns between weekdays and weekends?
+meandata$date <- as.Date(meandata$date)
+meandata$weekday <- weekdays(meandata$date)
+meandata$weekend <- ifelse(meandata$weekday=="Saturday" | meandata$weekday=="Sunday", "Weekend", "Weekday" )
+
+library(ggplot2)
+meandataweekendweekday <- aggregate(meandata$steps , by= list(meandata$weekend, meandata$interval), na.omit(mean))
+names(meandataweekendweekday) <- c("weekend", "interval", "steps")
+ggplot(meandataweekendweekday, aes(x=interval, y=steps, color=weekend)) + geom_line()+
+facet_grid(weekend ~.) + xlab("Interval") + ylab("Mean of Steps") +
+    ggtitle("Comparison of Average Number of Steps in Each Interval")
