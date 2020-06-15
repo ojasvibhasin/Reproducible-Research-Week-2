f3bd07eea817d9cc4d5497b5b7d3344685eb75c3
ojasvibhasin <65826071+ojasvibhasin@users.noreply.github.com>
2020-06-15 22:50:57 +0530

    Update and rename Stepsperday.R to 2.Calculate total number of steps taken each day.R

---
+++ 2.Calculate total number of steps taken each day.R
@@ -0,0 +1,5 @@
+2. Calculate total number of steps taken each day
+library(magrittr)
+library(dplyr)
+databydate <- stepdata %>% select(date, steps) %>% group_by(date) %>% summarize(tsteps= sum(steps)) %>%na.omit()
+hist(databydate$tsteps, xlab = "Total daily Steps",main="Histogram of Total Steps by day", breaks = 20)
